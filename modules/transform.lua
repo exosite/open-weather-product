1658583fcad6f31a5e90c39f7e3dcc73784b60e3
Cola Cheng <colacheng@exosite.com>
2019-06-27 16:08:38 +0800

    Fix transform example. (#27)

--- modules/transform.lua
+++ modules/transform.lua
@@ -2,23 +2,36 @@ local transform = {}
 
 -- Below an example of transforming the GPS data values
 
+-- function convertGps(data_in_source)
+--   local data_in, err = from_json(data_in_source)
+--   if err ~= nil then
+--     return data_in_source
+--   end
+
+--   if data_in.gps ~= nil then
+--     if data_in.gps.lat ~= nil then
+--       data_in.gps.lat = data_in.gps.lat / 1000000
+--     end
+--     if data_in.gps.lng ~= nil then
+--       data_in.gps.lng = data_in.gps.lng / 1000000
+--     end
+--   end
+
+--   return to_json(data_in)
+-- end
+
 -- function transform.convertIdentityState(state)
 --   if state == nil or state.data_in == nil then
 --     return state
 --   end
---
---   local data_in = from_json(state.data_in)
---
---   if data_in.gps ~= nil then
---     if data_in.lat ~= nil then
---       data_in.lat = data_in.lat / 100
---     end
---     if data_in.lng ~= nil then
---       data_in.lng = data_in.lng / 100
---     end
+
+--   if state.data_in.reported == nil then
+--     state.data_in = convertGps(state.data_in)
+--   else
+--     state.data_in.reported = convertGps(state.data_in.reported)
+--     state.data_in.set = convertGps(state.data_in.set)
 --   end
---
---   state.data_in = to_json(data_in)
+
 --   return state
 -- end
 
--- services/device2/event.lua
+++ services/device2/event.lua
@@ -1,7 +1,11 @@
 
 local transform = require("transform")
 if event.payload ~= nil and transform ~= nil and transform.convertIdentityState ~= nil then
-  event.payload = transform.convertIdentityState(event.payload)
+  for i, data in ipairs(event.payload) do
+    if data.values ~= nil and data.values.data_in ~= nil then
+      event.payload[i] = transform.convertIdentityState(data.values)
+    end
+  end
 end
 
 return Interface.trigger({event="event", data=event})
