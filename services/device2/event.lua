e39fb80ad4458b686b474795dfcf7cc0971b41df
Cola Cheng <colacheng@exosite.com>
2019-06-28 11:46:39 +0800

    Fix event transform lost values layer. (#28)

--- services/device2/event.lua
+++ services/device2/event.lua
@@ -3,7 +3,7 @@ local transform = require("transform")
 if event.payload ~= nil and transform ~= nil and transform.convertIdentityState ~= nil then
   for i, data in ipairs(event.payload) do
     if data.values ~= nil and data.values.data_in ~= nil then
-      event.payload[i] = transform.convertIdentityState(data.values)
+      event.payload[i].values = transform.convertIdentityState(data.values)
     end
   end
 end
